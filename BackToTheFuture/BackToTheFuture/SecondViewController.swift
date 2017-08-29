b1924ec556722266ccc24a067665a400330b28a4
Jose Melendez <JoseM_Melendez@icloud.com>
2017-08-29 00:49:24 -0400

    Wired up the UI in the secondViewController

--- BackToTheFuture/BackToTheFuture/SecondViewController.swift
+++ BackToTheFuture/BackToTheFuture/SecondViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var labe1: UILabel!
+    @IBOutlet weak var lab2: UILabel!
+    @IBOutlet weak var lab3: UILabel!
+    @IBOutlet weak var lab4: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func ButtonClicked(_ sender: Any) {
+    }
 }
